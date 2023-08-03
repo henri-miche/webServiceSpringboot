3732f106ab013ab15fff7c6101035329ba58860a
Henri Michel <h-michel@hotmail.com>
2023-08-03 18:08:24 -0300

    Project created

---
+++ src/main/java/com/michel/webservicespringboot/WebservicespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.michel.webservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicespringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michel/webservicespringboot/WebservicespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michel.webservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
